dfa413b1d7a458e6a314c8959c632413b5f2719f
Chisato Murakami <kiwipom1031@gmail.com>
2020-05-22 09:15:01 -0700

    pass tests that shows all saved projects

--- lib/project.rb
+++ lib/project.rb
@@ -18,4 +18,15 @@ class Project
     end
   end
 
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do | project |
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
 end
